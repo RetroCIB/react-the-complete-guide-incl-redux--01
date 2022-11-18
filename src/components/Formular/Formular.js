e8435edbc8eb9592c5e0e1681abfd354094b8930
RetroCIB <retrocib@gmail.com>
2022-11-18 19:44:10 +0200

    use one state example

--- src/components/Formular/Formular.js
+++ src/components/Formular/Formular.js
@@ -4,39 +4,55 @@ import './Formular.css';
 
 const Formular = () => {
 
-    const [titleValue, setTitleValue] = useState('');
-    const [priceValue, setPriceValue] = useState(0.01);
-    const [dateValue, setDateValue] = useState('');
-
+    const [formular, setFormular] = useState({
+        titleValue: '',
+        priceValue: '',
+        dateValue: '',
+    })
 
     const submitHandler = (event) => {
         event.preventDefault();
-    } 
+    }
     const changeTitleHandler = (event) => {
-        setTitleValue(event.target.value);
+        setFormular((prevState) => {
+            return {
+                ...prevState,
+                titleValue: event.target.value,
+            }
+        });
     }
     const changePriceHandler = (event) => {
-        setPriceValue(event.target.value);
+        setFormular((prevState) => {
+            return {
+                ...prevState,
+                priceValue: event.target.value,
+            }
+        });
     }
     const changeDateHandler = (event) => {
-        setDateValue(event.target.value);
+        setFormular((prevState) => {
+            return {
+                ...prevState,
+                dateValue: event.target.value,
+            }
+        });
     }
 
     return (
-
         <form className="formular" onSubmit={submitHandler}>
             <div className="formular-fields">
                 <div className="formular-field">
                     <label>title</label>
-                    <input type="text" value={titleValue} onChange={changeTitleHandler}/>
+                    <input type="text" value={formular.titleValue} onChange={changeTitleHandler}/>
                 </div>
                 <div className="formular-field">
                     <label>price</label>
-                    <input type="number" step="0.01" min="0.01" value={priceValue} onChange={changePriceHandler}/>
+                    <input type="number" step="0.01" min="0.01" value={formular.priceValue}
+                           onChange={changePriceHandler}/>
                 </div>
                 <div className="formular-field">
                     <label>date</label>
-                    <input type="date" value={dateValue} onChange={changeDateHandler}/>
+                    <input type="date" value={formular.dateValue} onChange={changeDateHandler}/>
                 </div>
                 <div className="formular-action">
                     <button>add item</button>
