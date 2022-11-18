c1d607e8ab088b92bb4e6c8e1dd226927a86d547
RetroCIB <retrocib@gmail.com>
2022-11-18 20:42:57 +0200

    refactoring formular

--- src/components/Formular/Formular.js
+++ src/components/Formular/Formular.js
@@ -2,61 +2,14 @@ import React, {useState} from "react";
 
 import './Formular.css';
 
+import Form from "./Form";
+
 const Formular = () => {
 
-    const [titleValue, setTitleValue] = useState('');
-    const [priceValue, setPriceValue] = useState('');
-    const [dateValue, setDateValue] = useState('');
-
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-
-        const itemData = {
-            title: titleValue,
-            price: priceValue,
-            date: new Date(dateValue)
-        }
-
-        setTitleValue('');
-        setPriceValue('');
-        setDateValue('');
-
-
-        console.log(itemData);
-    } 
-    const changeTitleHandler = (event) => {
-        setTitleValue(event.target.value);
-    }
-    const changePriceHandler = (event) => {
-        setPriceValue(event.target.value);
-    }
-    const changeDateHandler = (event) => {
-        setDateValue(event.target.value);
-    }
-
     return (
-
-        <form className="formular" onSubmit={submitHandler}>
-            <div className="formular-fields">
-                <div className="formular-field">
-                    <label>title</label>
-                    <input type="text" value={titleValue} onChange={changeTitleHandler}/>
-                </div>
-                <div className="formular-field">
-                    <label>price</label>
-                    <input type="number" step="0.01" min="0.01" value={priceValue} onChange={changePriceHandler}/>
-                </div>
-                <div className="formular-field">
-                    <label>date</label>
-                    <input type="date" value={dateValue} onChange={changeDateHandler}/>
-                </div>
-                <div className="formular-action">
-                    <button>add item</button>
-                </div>
-            </div>
-
-        </form>
+        <div className="formular">
+            <Form></Form>
+        </div>
     )
 }
 
